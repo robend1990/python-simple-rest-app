4c00aa6c77969786cfd48076517dc5a0b5b5a26a
Robert Drewniak <robert.drewniak@mclaren.com>
2020-04-27 08:55:30 +0200

    Added association model

--- cvapp/models.py
+++ cvapp/models.py
@@ -1,10 +1,20 @@
 from cvapp import db
 
+
+class UserSkillAssociation(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    skill_id = db.Column(db.Integer, db.ForeignKey('skill.id'), primary_key=True)
+    level = db.Column(db.Integer, nullable=False)
+    skill = db.relationship('Skill')
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     last_name = db.Column(db.String(100), nullable=False)
     first_name = db.Column(db.String(100), nullable=False)
     cv_url = db.Column(db.String(255), nullable=True)
+    skillAssociations = db.relationship('UserSkillAssociation', lazy=True, cascade="all, delete-orphan")
+
 
 class Skill(db.Model):
     id = db.Column(db.Integer, primary_key=True)
